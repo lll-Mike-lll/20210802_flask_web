676a5608c81e25ae5b3865aad9e868273de79336
mm <mm@mm.local>
2021-08-02 14:10:30 +0700

    mike

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-import flask
+from flask import Flask
 
 
-# Create the application.
-APP = flask.Flask(__name__)
+app = Flask(__name__)
 
 
-@APP.route('/')
+@app.route('/')
 def index():
     """ Displays the index page accessible at '/'
     """
     return flask.render_template('index.html')
 
-@APP.route('/04_contact')
+@app.route('/04_contact')
 def contact():
     return flask.render_template('04_contact.html')
 
-@APP.route('/03_about')
+@app.route('/03_about')
 def about():
     return flask.render_template('03_about.html')
 
-@APP.route('/02_product')
+@app.route('/02_product')
 def product():
     return flask.render_template('02_product.html')
 
-@APP.route('/01_project')
+@app.route('/01_project')
 def project():
     return flask.render_template('01_project.html')
 
 
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
 if __name__ == '__main__':
-    APP.debug = True
-    APP.run(host='127.0.0.1', port=8080, debug=True)
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
