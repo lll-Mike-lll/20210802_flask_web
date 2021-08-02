065ce6b0db31f74a6e085d87725e19371f97dd81
mm <mm@mm.local>
2021-08-02 15:11:37 +0700

    mike

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+import flask 
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
@@ -27,14 +27,6 @@ def project():
     return flask.render_template('01_project.html')
 
 
-
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
 
 
 if __name__ == '__main__':
